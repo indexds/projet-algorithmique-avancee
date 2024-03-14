8b34b5f47071ff7cc0bc611d8be26e9041529b33
_index <indexd@pm.me>
2024-03-14 14:04:11 +0100

    final app look

--- app.c
+++ app.c
@@ -1,16 +1,16 @@
-#include "app.h"
+#include <gtk/gtk.h>
 
 static void activate(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window;
     GtkWidget *grid;
     GtkWidget *textbox1, *textbox2;
-    GtkWidget *image;
+    GtkWidget *button_find_animal, *button_find_chars;
     (void)user_data;
 
     window = gtk_application_window_new(GTK_APPLICATION(app));
-    gtk_window_set_title(GTK_WINDOW(window), "Akinator mais que pour les animaux");
-    gtk_window_set_default_size(GTK_WINDOW(window), 750, 750);
+    gtk_window_set_title(GTK_WINDOW(window), "Akinator de Aldi");
+    gtk_window_set_default_size(GTK_WINDOW(window), 310, 90);
 
     grid = gtk_grid_new();
     gtk_window_set_child(GTK_WINDOW(window), grid);
@@ -18,14 +18,18 @@ static void activate(GtkApplication *app, gpointer user_data)
     textbox1 = gtk_entry_new();
     gtk_grid_attach(GTK_GRID(grid), textbox1, 0, 0, 1, 1);
 
-    image = gtk_image_new_from_file("dog.jpg");
-    gtk_widget_set_size_request(image, 300, 300);
-    gtk_grid_attach(GTK_GRID(grid), image, 1, 0, 1, 1);
+    // Button "Find Animal"
+    button_find_animal = gtk_button_new_with_label("Find Animal");
+    gtk_grid_attach(GTK_GRID(grid), button_find_animal, 0, 1, 1, 1);
+
+    // Button "Find Chars"
+    button_find_chars = gtk_button_new_with_label("Find Characs.");
+    gtk_grid_attach(GTK_GRID(grid), button_find_chars, 2, 1, 1, 1);
 
     textbox2 = gtk_entry_new();
     gtk_grid_attach(GTK_GRID(grid), textbox2, 2, 0, 1, 1);
 
-    gtk_window_present(GTK_WINDOW(window));
+    gtk_widget_set_visible(window, true); // Show all widgets
 }
 
 int app(void)
